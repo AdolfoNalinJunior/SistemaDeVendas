9b911319957a9e3d64229cc66d77ba80b79f415a
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-07-10 13:38:05 -0300

    Adição e configurações dos elementos na tela Listar

--- CamadaApresentacao/frmCategoporia.Designer.cs
+++ CamadaApresentacao/frmCategoporia.Designer.cs
@@ -31,7 +31,18 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.bntDeletar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.chkDeletar = new System.Windows.Forms.CheckBox();
+            this.dataListar = new System.Windows.Forms.DataGridView();
+            this.Deletar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataListar)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -46,12 +57,20 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.dataListar);
+            this.tabPage1.Controls.Add(this.chkDeletar);
+            this.tabPage1.Controls.Add(this.lblTotal);
+            this.tabPage1.Controls.Add(this.btnImprimir);
+            this.tabPage1.Controls.Add(this.bntDeletar);
+            this.tabPage1.Controls.Add(this.btnBuscar);
+            this.tabPage1.Controls.Add(this.txtBuscar);
+            this.tabPage1.Controls.Add(this.lblNome);
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(777, 407);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Categoria";
+            this.tabPage1.Text = "Listar";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -64,6 +83,94 @@
             this.tabPage2.Text = "Configurações de Itens";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(23, 35);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(40, 15);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(69, 32);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(189, 23);
+            this.txtBuscar.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(275, 31);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // bntDeletar
+            // 
+            this.bntDeletar.Location = new System.Drawing.Point(356, 31);
+            this.bntDeletar.Name = "bntDeletar";
+            this.bntDeletar.Size = new System.Drawing.Size(75, 23);
+            this.bntDeletar.TabIndex = 7;
+            this.bntDeletar.Text = "Deletar ";
+            this.bntDeletar.UseVisualStyleBackColor = true;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(437, 31);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 8;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(655, 335);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(45, 21);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total";
+            // 
+            // chkDeletar
+            // 
+            this.chkDeletar.AutoSize = true;
+            this.chkDeletar.Location = new System.Drawing.Point(23, 96);
+            this.chkDeletar.Name = "chkDeletar";
+            this.chkDeletar.Size = new System.Drawing.Size(63, 19);
+            this.chkDeletar.TabIndex = 10;
+            this.chkDeletar.Text = "Deletar";
+            this.chkDeletar.UseVisualStyleBackColor = true;
+            // 
+            // dataListar
+            // 
+            this.dataListar.AllowUserToAddRows = false;
+            this.dataListar.AllowUserToDeleteRows = false;
+            this.dataListar.AllowUserToOrderColumns = true;
+            this.dataListar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataListar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Deletar});
+            this.dataListar.Location = new System.Drawing.Point(23, 121);
+            this.dataListar.MultiSelect = false;
+            this.dataListar.Name = "dataListar";
+            this.dataListar.ReadOnly = true;
+            this.dataListar.RowTemplate.Height = 25;
+            this.dataListar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataListar.Size = new System.Drawing.Size(693, 211);
+            this.dataListar.TabIndex = 11;
+            // 
+            // Deletar
+            // 
+            this.Deletar.HeaderText = "Deletar";
+            this.Deletar.Name = "Deletar";
+            this.Deletar.ReadOnly = true;
+            this.Deletar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Deletar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
             // frmCategoporia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -74,6 +181,9 @@
             this.Text = "Categoria";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataListar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +193,14 @@
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
+        private DataGridView dataListar;
+        private DataGridViewCheckBoxColumn Deletar;
+        private CheckBox chkDeletar;
+        private Label lblTotal;
+        private Button btnImprimir;
+        private Button bntDeletar;
+        private Button btnBuscar;
+        private TextBox txtBuscar;
+        private Label lblNome;
     }
 }
