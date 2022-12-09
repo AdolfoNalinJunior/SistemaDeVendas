d8af59cdafbaa12b8a3f64cc618385ff4fce41d9
Adolfo Nalin Junior <adolfonalin22@gmail.com>
2022-12-09 06:12:50 -0300

    Atualização do nome do banco de dados, essa atualização foi necessaria devida a troca de sistema operacional

--- CamadaDado/Conexao.cs
+++ CamadaDado/Conexao.cs
@@ -9,6 +9,6 @@ namespace CamadaDado
     internal class Conexao
     {
         // Interação da classe com o banco de Dados
-        public static string Cn = "Data Source = DESKTOP-CIV07CI; Initial Catalog = dbcomercio; integrated Security = true";
+        public static string Cn = "Data Source = DESKTOP-B5FVB5M\\SQLEXPRESS; Initial Catalog = dbcomercio; integrated Security = true";
     }
 }
